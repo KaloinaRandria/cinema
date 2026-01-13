b1976c437b2ccd52bb47f226c298c7e444fd80c7
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2026-01-13 20:03:42 +0300

    init project

---
+++ src/test/java/mg/working/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.working.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
